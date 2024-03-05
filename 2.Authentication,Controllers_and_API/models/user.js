386ad6a5d6028e800b71317d5f415d19f38affdf
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-05 17:44:26 +0530

    Create user.js

---
+++ 2.Authentication,Controllers_and_API/models/user.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+  lastLoggedIn: { type: Date, default: Date.now },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+const User = model("User", userSchema);
+
+export default User;
