986addac9353f735061f6390e6df5a7c62fb63f0
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-07 23:33:16 +0530

    Add files via upload

---
+++ 2.Authentication,Controllers_and_API/3.Authentication_API-Controller_and_Routes/routes/api/signup-user.js
@@ -0,0 +1,11 @@
+import { signupUser } from "../../controllers/user.js";
+
+export default async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const { user, token } = await signupUser({ name, email, password });
+    res.json({ user, token });
+  } catch (error) {
+    res.status(403).json(error);
+  }
+};
