1290130284ddc3d59c8070862427122640e617da
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-07 23:37:00 +0530

    Add files via upload

---
+++ 2.Authentication,Controllers_and_API/4.Posts_API-Controller_and_Routes/routes/api/store-post.js
@@ -0,0 +1,15 @@
+import { createPost } from "../../controllers/post.js";
+
+export default async (req, res) => {
+  try {
+    const post = await createPost(req.body.post);
+    res.json({ post });
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
+
+// export default (req, res, next) => {
+//   console.log(req.body);
+//   res.json({ message: "Got it!" });
+// };
