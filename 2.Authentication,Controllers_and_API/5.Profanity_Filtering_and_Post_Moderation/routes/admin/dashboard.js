15be08cbc8c47fbc94b73373f8441b5c055a2c37
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-07 23:38:17 +0530

    Create dashboard.js

---
+++ 2.Authentication,Controllers_and_API/5.Profanity_Filtering_and_Post_Moderation/routes/admin/dashboard.js
@@ -0,0 +1,17 @@
+import moment from "moment";
+import { getFlaggedPosts } from "../../controllers/post.js";
+
+export default async (req, res) => {
+  try {
+    const getPosts = await getFlaggedPosts();
+    res.render("dashboard", {
+      user: req.session.user.name,
+      lastLoggedin: moment(req.session.user.lastLoggedIn).format(
+        "MMMM, do YYYY, h:mm:ss a"
+      ),
+      posts: getPosts,
+    });
+  } catch (error) {
+    res.send("There was an error while loading the page");
+  }
+};
