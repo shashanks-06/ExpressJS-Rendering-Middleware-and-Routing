c63c4df98e865ddb6e83c5e2ec9e7741b7364e91
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-12 23:52:27 +0530

    Create index.js

---
+++ 4.Protecting_Express_App/1.Handling_Untrusted_Data/routes/admin/index.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import protectRoute from "../../utils/protectRoute.js";
+import home from "./home.js";
+import login from "./login.js";
+import dashboard from "./dashboard.js";
+import logOut from "./logout.js";
+import moderatePost from "./moderate-post.js";
+import signupAdmin from "./signup-admin.js";
+import {
+  loginAdminValidation,
+  signUpAdminValidation,
+} from "../../utils/validation.js";
+
+const router = Router();
+
+router.get("/", home);
+router
+  .route("/login")
+  .get((req, res) => res.render("login"))
+  .post(loginAdminValidation, login);
+
+router
+  .route("/signup")
+  .get((req, res) => res.render("signup"))
+  .post(signUpAdminValidation, signupAdmin);
+router.get("/dashboard", protectRoute("/admin/login"), dashboard);
+router.get("/logout", logOut);
+router.post("/moderate", moderatePost);
+
+export default router;
