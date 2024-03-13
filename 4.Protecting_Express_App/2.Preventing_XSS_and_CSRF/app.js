7875d32afd8fe3ee07efcaca63ab0783762c6512
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-13 12:02:24 +0530

    Create app.js

---
+++ 4.Protecting_Express_App/2.Preventing_XSS_and_CSRF/app.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { createWriteStream } from "fs";
+import morgan from "morgan";
+import session from "express-session";
+import compression from "compression";
+import home from "./routes/home/index.js";
+import admin from "./routes/admin/index.js";
+import api from "./routes/api/index.js";
+import connectToDb from "./db/index.js";
+import helmet from "helmet";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const logFile = join(__dirname, "blogchef.log");
+
+app.use(helmet());
+app.use(compression());
+app.use("/assets", express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, "public", "client")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  "/admin",
+  session({
+    name: "sessId",
+    secret: process.env.sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: app.get("env") === "production" ? true : false,
+      httpOnly: true,
+      maxAge: 18000000, // 5 hours
+    },
+  })
+);
+app.use(morgan(":method - :url - :date - :response-time ms"));
+app.use(
+  morgan(":method - :url - :date - :response-time ms", {
+    stream: createWriteStream(logFile, { flags: "a" }),
+  })
+);
+
+app.set("view engine", "pug");
+
+app.use("/admin", admin);
+app.use("/api", api);
+app.use("/", home);
+
+Promise.all([connectToDb()])
+  .then(() =>
+    app.listen(3000, () => console.log("Blog Chef is cooking on port 3000"))
+  )
+  .catch((error) => {
+    console.error(`MongoDB Atlas Error: ${error}`);
+    process.exit();
+  });
